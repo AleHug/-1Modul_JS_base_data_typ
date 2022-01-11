3bcf0c733faed8a5e0096a0af8078e618aabaeed
AleHug <93007065+AleHug@users.noreply.github.com>
2022-01-11 10:33:52 +0100

    Add files via upload

---
+++ data-types.js
@@ -0,0 +1,36 @@
+// Number//
+const age = 30;
+console.log(age, 'age', typeof age);
+
+// String //
+const name = 'Alex';
+console.log(name, 'name', typeof name);
+
+// Boolean //
+const hungry = true;
+console.log(hungry, 'hungry', typeof hungry);
+
+// Null //
+const eyecolor = null;
+console.log(eyecolor, 'eyercolor', typeof eyecolor);
+
+// Undefined //
+const athlete = undefined;
+console.log(athlete, 'athlete', typeof athlete );
+
+// Object//
+const person = {
+    name: 'Alex',
+    age: 30,
+    hungry: true,
+};
+console.log(person);
+
+// bigint //
+const  bigintNumber = BigInt(10);
+console.log(bigintNumber, 'bigintNumber', typeof bigintNumber);
+
+// Symbol //  
+const id = Symbol('id');
+console.log(id);
+
---
+++ index.js
@@ -0,0 +1,15 @@
+// Number-String//
+let age = 30;
+age = '30';
+alert(age);
+
+// String-Number //
+let Alex = '25';
+Alex = 25;
+alert(Alex);
+
+// Boolean-Null //
+let hungry = true;
+hungry = null;
+alert(hungry);
+
